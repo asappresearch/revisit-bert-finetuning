d42e3dc77e02888cdf29b39d465f953ca1b54977
jmusiel <73840022+jmusiel@users.noreply.github.com>
2021-11-02 00:42:21 +0000

    added results and script changes to get them

--- sample_commands/reinit.sh
+++ sample_commands/reinit.sh
@@ -1,11 +1,11 @@
 # using bias correction
 python run_glue.py \
     --model_type bert --model_name_or_path bert-large-uncased --task_name RTE \
-    --do_train --data_dir /persist/data/glue_data/RTE --max_seq_length 128 \
+    --do_train --data_dir /home/jovyan/working/class_projects/nlp_11711_project/bert_finetuning_test/glue/glue_data/RTE --max_seq_length 128 \
     --per_gpu_eval_batch_size 64 --weight_decay 0 --seed 0 --fp16 \
     --overwrite_output_dir --do_lower_case --per_gpu_train_batch_size 32 \
     --gradient_accumulation_steps 1 --logging_steps 0 --num_loggings 10 \
-    --save_steps 0 --test_val_split --use_torch_adamw --cache_dir /home/ubuntu/hf-transformers-cache \
+    --save_steps 0 --test_val_split --use_torch_adamw --cache_dir /home/jovyan/working/class_projects/nlp_11711_project/bert_finetuning_test/cache \
     --num_train_epochs 3.0 --warmup_ratio 0.1 --learning_rate 2e-05 \
     --output_dir bert_output/REINIT5/RTE/SEED0 \
     --reinit_pooler --reinit_layers 5
